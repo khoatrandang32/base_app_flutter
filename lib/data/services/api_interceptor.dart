06069660888e644e2ffdf66f6873f1677d73b018
khoatrandang32 <khoatd.dev@gmail.com>
2025-07-11 23:43:28 +0700

    update logic save/get auth token

--- lib/data/services/api_interceptor.dart
+++ lib/data/services/api_interceptor.dart
@@ -1,3 +1,4 @@
+import 'package:base_app/core/services/preferences.dart';
 import 'package:dio/dio.dart';
 import 'package:logger/logger.dart';
 
@@ -5,16 +6,17 @@ class ApiInterceptor extends Interceptor {
   final logger = Logger(printer: PrettyPrinter(methodCount: 0));
 
   @override
-  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    final token = "Bearer YOUR_TOKEN"; 
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) async{
+    final token = await Prefs.getToken;
     options.headers['Authorization'] = token;
     //LOGGER
-    logger.i('${options.method} ${options.uri}\nBody: ${options.data}');
+    logger.i('${options.method} ${options.uri}\nHeader: ${options.headers}\nBody: ${options.data}');
     handler.next(options);
   }
 
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
+    //LOGGER
     logger.i(
       'Status ${response.statusCode}: ${response.requestOptions.method} ${response.requestOptions.uri}\n'
       '${response.data}',
